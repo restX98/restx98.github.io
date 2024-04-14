3679b1874821914beff869f0c169aef7c0ce502c
Enrico Restuccia <enrico.rest98@gmail.com>
2024-04-14 14:16:11 +0200

    Snake
    Random walking

--- src/context/snake-game-context.jsx
+++ src/context/snake-game-context.jsx
@@ -6,6 +6,13 @@ import { useInterval } from '@/lib/utils'
 
 const SnakeGameContext = createContext()
 
+const Directions = {
+  UP: 'UP',
+  RIGHT: 'RX',
+  DOWN: 'DW',
+  LEFT: 'LX'
+}
+
 // Provider component to wrap the entire game and provide the game state
 export const SnakeGameProvider = ({ children }) => {
   const [tileSize, setTileSize] = useState(40)
@@ -17,7 +24,7 @@ export const SnakeGameProvider = ({ children }) => {
   const [snake, setSnake] = useState([{ x: 9, y: 10 }, { x: 8, y: 10 }, { x: 7, y: 10 }, { x: 6, y: 10 }, { x: 5, y: 10 }])
   const [foods, setFoods] = useState([{ x: 10, y: 15 }])
 
-  const [direction, setDirection] = useState('RX')
+  const [direction, setDirection] = useState(Directions.RIGHT)
   const [changeCount, setChangeCount] = useState(5)
 
   useEffect(() => {
@@ -53,26 +60,45 @@ export const SnakeGameProvider = ({ children }) => {
     setSnake(prevSnake => [...prevSnake, prevSnake.at(-1)])
   }
 
-  function getNewHead() {
+  const pickRandomDirection = () => {
+    if (direction === Directions.UP || direction === Directions.DOWN) {
+      return [Directions.LEFT, Directions.RIGHT].at(Math.floor(Math.random() * 2))
+    } else {
+      return [Directions.UP, Directions.DOWN].at(Math.floor(Math.random() * 2))
+    }
+  }
+
+  const updateDirection = () => {
+    setChangeCount(prev => prev - 1)
+
+    if (changeCount === 0) {
+      setChangeCount(Math.floor(Math.random() * 15) + 1)
+      setDirection(pickRandomDirection())
+    }
+  }
+
+  const getInRangeValue = (value, max) => {
+    const range = max + 1
+    return (value % range + range) % range
+  }
+
+  const getNewHead = () => {
     const head = snake.at(0)
     switch (direction) {
       case 'UP':
-        return { x: head.x, y: (head.y + 1) % gridDimension.rows }
+        return { x: head.x, y: getInRangeValue(head.y - 1, gridDimension.rows - 1) }
       case 'RX':
-        return { x: (head.x + 1) % gridDimension.cols, y: head.y }
+        return { x: getInRangeValue(head.x + 1, gridDimension.cols - 1), y: head.y }
       case 'DW':
-        return { x: head.x, y: (head.y - 1) % gridDimension.rows }
+        return { x: head.x, y: getInRangeValue(head.y + 1, gridDimension.rows - 1) }
       case 'LX':
-        return { x: (head.x - 1) % gridDimension.cols, y: head.y }
+        return { x: getInRangeValue(head.x - 1, gridDimension.cols - 1), y: head.y }
       default:
-        return { x: (head.x + 1) % gridDimension.cols, y: head.y }
+        return { x: getInRangeValue(head.x + 1, gridDimension.cols - 1), y: head.y }
     }
-
   }
 
   const updateSnake = () => {
-    const newHead = getNewHead()
-
     const tail = snake.at(-1)
     const isDigested = foods.findIndex((f) => f.x === tail.x && f.y === tail.y)
 
@@ -80,6 +106,9 @@ export const SnakeGameProvider = ({ children }) => {
       setFoods((prevFood) => prevFood.filter((_, i) => i !== isDigested))
       addSnakeTail()
     }
+
+    updateDirection()
+    const newHead = getNewHead()
 
     setSnake((prevSnake) => {
       const newSnake = [newHead, ...prevSnake.slice(0, -1)]
