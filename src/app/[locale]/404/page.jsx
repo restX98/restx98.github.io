7c99ec7fa27a327eb75e7a82a202563509692903
Enrico Restuccia <enrico.rest98@gmail.com>
2024-06-16 20:38:15 +0200

    Not Found Page
    Fixes

--- src/app/[locale]/404/page.jsx
+++ src/app/[locale]/404/page.jsx
@@ -13,17 +13,17 @@ export default function NotFound() {
         <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
           <div className="mx-auto max-w-screen-sm text-center">
             <h1 className="mb-4 text-7xl font-extrabold tracking-tight text-lime-600 lg:text-9xl">
-              {t('404')}
+              {t("404")}
             </h1>
             <p className="mb-4 text-3xl font-bold tracking-tight text-white md:text-4xl">
-              {t('message')}
+              {t("message")}
             </p>
 
             <Link
               className="my-4 inline-flex rounded-lg bg-zinc-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-zinc-700"
               href="/"
             >
-              {t('backToHome')}
+              {t("backToHome")}
             </Link>
           </div>
         </div>
